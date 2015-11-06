2c4f8282b6c7fbd6d3ba8308fb6f0885288ae678
Benji91 <benjamin.jahic.001@student.uni.lu>
2015-11-06 12:16:35 +0100

    Nothing Special

--- DriveClever/APIAccess.swift
+++ DriveClever/APIAccess.swift
@@ -64,7 +64,6 @@ class APIAccess{
         
         /*Handling the result optained of the function call*/
         task.continueWithBlock({ (task: AWSTask!) -> AWSTask! in
-
             if (task.error != nil) { // NO ERROR OCCURES
                 NSLog("Invoke Lambda returned an error : \(task.error)")
                 dispatch_async(dispatch_get_main_queue(), {
@@ -76,17 +75,18 @@ class APIAccess{
             }
             else {
                 if (task.result != nil) { //WE HAVE A KNOWN RESULT
-                   
+                    
                     NSLog("Invoke Lambda : result = \(task.result)")
                     //upate text label on the main UI thread if there is anyy
                     setResultTask(task)
-
+                    
                     dispatch_async(dispatch_get_main_queue(), {
                         //let r = task.result as! Dictionary<String,String>
                         //self.greeting.text = r["message"]
                         //self.deviceType.text = r["device"]
                         //SVProgressHUD.dismiss()
                     })
+                    return task
                     
                 }
                 else { //WE HAVE A UNKNOWS RESULT
@@ -100,20 +100,13 @@ class APIAccess{
                     })
                 }
             }
-
             return nil
         })
         
-       sleep(1)
-        
-        
-        
+        sleep(5)
         /*Final RESULT*/
-        print("Invoke 2 Lambda : result = \(resultTask.result)")
-
+        print("Invoke 2 Lambda : result = \(task.result)")
         
-
-
     }
     
     class func requestSituations() -> String{
@@ -124,54 +117,55 @@ class APIAccess{
         
         /*New task to call the testAPI function (the function call)*/
         let task = lambdaInvoker.invokeFunction("testAPI",JSONObject: [])
-
+        
         /*Handling the result optained of the function call*/
         task.continueWithBlock(
             {
-        
-            (task: AWSTask!) -> AWSTask! in
-            
-            if (task.error != nil) { // NO ERROR OCCURES
-                NSLog("Invoke Lambda returned an error : \(task.error)")
-                dispatch_async(dispatch_get_main_queue(), {
-                    //self.greeting.text = "Error"
-                    //self.deviceType.text = task.error.description
-                    //SVProgressHUD.dismiss()
-                })
-            }
-            else {
-                if (task.result != nil) { //WE HAVE A KNOWN RESULT
-                    
-                    NSLog("Invoke Lambda : result = \(task.result)")
-                    
-                    //upate text label on the main UI thread if there is anyy
-                    dispatch_async(dispatch_get_main_queue(), {
-                        //let r = task.result as! Dictionary<String,String>
-                        //self.greeting.text = r["message"]
-                        //self.deviceType.text = r["device"]
-                        //SVProgressHUD.dismiss()
-                    })
-                    
-                }
-                else { //WE HAVE A UNKNOWS RESULT
-                    
-                    NSLog("Invoke Lambda : unknow result : \(task)");
-                    NSLog("Exception : \(task.exception)")
-                    NSLog("Error : \(task.error)" )
-                    
+                
+                (task: AWSTask!) -> AWSTask! in
+                
+                if (task.error != nil) { // NO ERROR OCCURES
+                    NSLog("Invoke Lambda returned an error : \(task.error)")
                     dispatch_async(dispatch_get_main_queue(), {
                         //self.greeting.text = "Error"
+                        //self.deviceType.text = task.error.description
                         //SVProgressHUD.dismiss()
                     })
                 }
-            }
-            return task
+                else {
+                    if (task.result != nil) { //WE HAVE A KNOWN RESULT
+                        
+                        NSLog("Invoke Lambda : result = \(task.result)")
+                        
+                        //upate text label on the main UI thread if there is anyy
+                        dispatch_async(dispatch_get_main_queue(), {
+                            //let r = task.result as! Dictionary<String,String>
+                            //self.greeting.text = r["message"]
+                            //self.deviceType.text = r["device"]
+                            //SVProgressHUD.dismiss()
+                        })
+                        
+                    }
+                    else { //WE HAVE A UNKNOWS RESULT
+                        
+                        NSLog("Invoke Lambda : unknow result : \(task)");
+                        NSLog("Exception : \(task.exception)")
+                        NSLog("Error : \(task.error)" )
+                        
+                        dispatch_async(dispatch_get_main_queue(), {
+                            //self.greeting.text = "Error"
+                            //SVProgressHUD.dismiss()
+                        })
+                    }
+                }
+                return task
         })
         
-
+        
         return ""
-
+        
     }
+
     
     class func requestArea() {
         
